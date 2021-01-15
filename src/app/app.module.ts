c0f3fe10f9fb69aac41895fea59d1be7a2f7fe27
Pavel Gendelman <pavelgendelman@coherentsolutions.com>
2021-01-15 19:24:36 +0300

    routing + pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './pages/index/index.component';
+import { ConfigComponent } from './pages/config/config.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    ConfigComponent
   ],
   imports: [
     BrowserModule,
