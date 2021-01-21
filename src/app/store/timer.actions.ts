706fb0ec5444d924914b50150f012529d837bf3c
Pavel Gendelman <pavelgendelman@coherentsolutions.com>
2021-01-21 14:52:12 +0300

    reset action added for timer

--- src/app/store/timer.actions.ts
+++ src/app/store/timer.actions.ts
@@ -3,3 +3,5 @@ import { createAction } from '@ngrx/store';
 export const setActive = createAction('[Timer] Active');
 
 export const setOnPause = createAction('[Timer] Paused');
+
+export const reset = createAction('[Timer] Reseted');
