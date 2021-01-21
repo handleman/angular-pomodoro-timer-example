d8f6032d35b18fc8392f5a55d8206e3e383d0b61
Pavel Gendelman <pavelgendelman@coherentsolutions.com>
2021-01-21 15:40:08 +0300

    timer actions connected to buttons

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { Timer, CountDown } from '../../store/types';
+
+import { setActive, setOnPause, reset } from '../../store/timer.actions'
+
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
@@ -7,9 +14,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+  timer$: Observable<Timer>;
+  countDown$: Observable<CountDown>;
+
+  isTimerReadyForUse: boolean = false;
+  isTimerInUse: boolean = false;
+  isTimerOnPause: boolean = false;
+
+  constructor(private store: Store<{ timer: Timer, countDown: CountDown }>) {
+    this.timer$ = store.select('timer');
+    this.countDown$ = store.select('countDown');
+  }
 
   ngOnInit(): void {
+    this.timer$.subscribe((data) => {
+      const { active, paused } = data;
+      this.isTimerReadyForUse = (!active && !paused);
+      this.isTimerInUse = (active && !paused);
+      this.isTimerOnPause = (active && paused);
+    }); 
+  }
+
+  handleStartTimer(): void {
+    console.log('timer started');
+    this.store.dispatch(setActive());
+  }
+  handlePauseTimer(): void {
+    console.log('timer paused');
+    this.store.dispatch(setOnPause());
+  }
+  handleResetTimer(): void {
+    console.log('timer paused');
+    this.store.dispatch(reset());
   }
 
 }
