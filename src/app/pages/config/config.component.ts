e9f6e563b24e18367486c810eebf1c2cddcf247f
handleman <pavel.handleman@gmail.com>
2021-01-24 14:13:24 +0300

    config page

--- src/app/pages/config/config.component.ts
+++ src/app/pages/config/config.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { CountDown } from '../../store/types';
+import { setInterval, setRest } from '../../store/countDown.actions';
 
 @Component({
   selector: 'app-config',
   templateUrl: './config.component.html',
   styleUrls: ['./config.component.scss']
 })
-export class ConfigComponent implements OnInit {
+export class ConfigComponent {
+  countDown$: Observable<CountDown>;
 
-  constructor() { }
+  constructor(private store: Store<{ countDown: CountDown }>) {
+    this.countDown$ = store.select('countDown');
 
-  ngOnInit(): void {
+  }
+
+  onIntervalChange(value): void {
+    console.log('interval: ', value);
+    this.store.dispatch(setInterval({ payload: value }));
+
+  }
+  onRestChange(value): void {
+    console.log('rest: ', value);
+    this.store.dispatch(setRest({ payload: value }));
   }
 
 }
